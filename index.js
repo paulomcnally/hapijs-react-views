c87ccca32534c2adff0aaf2c744956987bcefeb3
Paulo McNally <paulomcnally@gmail.com>
2015-09-06 11:05:54 -0600

    adapted to hapi

--- index.js
+++ index.js
@@ -17,56 +17,57 @@ var DEFAULT_OPTIONS = {
   transformViews: true,
 };
 
-function createEngine(engineOptions) {
+module.exports = function (engineOptions) {
   var registered = false;
   var moduleDetectRegEx;
 
   engineOptions = assign({}, DEFAULT_OPTIONS, engineOptions || {});
 
-  function renderFile(filename, options, cb) {
-    // Defer babel registration until the first request so we can grab the view path.
-    if (!moduleDetectRegEx) {
-      moduleDetectRegEx = new RegExp('^' + options.settings.views);
-    }
-    if (engineOptions.transformViews && !registered) {
-      // Passing a RegExp to Babel results in an issue on Windows so we'll just
-      // pass the view path.
-      require('babel/register')({
-        only: options.settings.views
-      });
-      registered = true;
-    }
-
-    try {
-      var markup = engineOptions.doctype;
-      var component = require(filename);
-      // Transpiled ES6 may export components as { default: Component }
-      component = component.default || component;
-      markup +=
-        React.renderToStaticMarkup(React.createElement(component, options));
-    } catch (e) {
-      return cb(e);
-    }
-
-    if (engineOptions.beautify) {
-      // NOTE: This will screw up some things where whitespace is important, and be
-      // subtly different than prod.
-      markup = beautifyHTML(markup);
-    }
-
-    if (options.settings.env === 'development') {
-      // Remove all files from the module cache that are in the view folder.
-      Object.keys(require.cache).forEach(function(module) {
-        if (moduleDetectRegEx.test(require.cache[module].filename)) {
-          delete require.cache[module];
+  return {
+    module: {
+      compile: function (filename, options, cb) {
+        // Defer babel registration until the first request so we can grab the view path.
+        if (!moduleDetectRegEx) {
+          moduleDetectRegEx = new RegExp('^' + options.settings.views);
         }
-      });
+        if (engineOptions.transformViews && !registered) {
+          // Passing a RegExp to Babel results in an issue on Windows so we'll just
+          // pass the view path.
+          require('babel/register')({
+            only: options.settings.views
+          });
+          registered = true;
+        }
+
+        try {
+          var markup = engineOptions.doctype;
+          var component = require(filename);
+          // Transpiled ES6 may export components as { default: Component }
+          component = component.default || component;
+          markup +=
+            React.renderToStaticMarkup(React.createElement(component, options));
+        } catch (e) {
+          return cb(e);
+        }
+
+        if (engineOptions.beautify) {
+          // NOTE: This will screw up some things where whitespace is important, and be
+          // subtly different than prod.
+          markup = beautifyHTML(markup);
+        }
+
+        if (options.settings.env === 'development') {
+          // Remove all files from the module cache that are in the view folder.
+          Object.keys(require.cache).forEach(function(module) {
+            if (moduleDetectRegEx.test(require.cache[module].filename)) {
+              delete require.cache[module];
+            }
+          });
+        }
+
+        cb(null, markup);
+      },
+      compileMode: 'async'
     }
-
-    cb(null, markup);
   }
-
-  return renderFile;
 }
-
-module.exports = createEngine
